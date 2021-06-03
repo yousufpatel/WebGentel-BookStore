2cfc333a3dfbb8febdcbb929464c843d7013446b
Md Yousuf Patel <Md_yousuf_Patel@Dellteam.com>
2021-06-03 22:00:20 +0530

    Added a book  controller and repository

---
+++ src/WebGentel-BookStore/Controllers/BookController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebGentel_BookStore.Models;
+using WebGentel_BookStore.Repository;
+
+namespace WebGentel_BookStore.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly BookRepository _bookRepository;
+        public BookController()
+        {
+            _bookRepository = new BookRepository();
+        }
+        public IActionResult GetAllBooks()
+        {
+            return Json(_bookRepository.GetAllBook());
+        }
+
+        public BookModel GetBook(int id)
+        {
+            return _bookRepository.GetBookById(id);
+        }
+
+        public List<BookModel> SearchBookName(string bookName, string autherName)
+        {
+            return _bookRepository.SearchBook(bookName, autherName);
+
+        }
+    }
+}
---
+++ src/WebGentel-BookStore/Repository/BookRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using WebGentel_BookStore.Models;
+
+namespace WebGentel_BookStore.Repository
+{
+    public class BookRepository
+    {
+        public List<BookModel> GetAllBook()
+        {
+            return DataSource();
+        }
+        public BookModel GetBookById(int id)
+        {
+            return DataSource().Where(x => { return x.Id == 1; }).FirstOrDefault();
+        }
+        public List<BookModel> SearchBook(string title, string authorName)
+        {
+            return DataSource().Where(x => x.Title.Contains(title, StringComparison.InvariantCultureIgnoreCase) ||
+                                      x.Author.Contains(authorName, StringComparison.InvariantCultureIgnoreCase)).ToList();
+        }
+
+        private List<BookModel> DataSource()
+        {
+            return new List<BookModel>
+            {
+                new BookModel { Id =  1, Author = "Yousuf", Title = "MVC"},
+                new BookModel { Id = 2, Author = "Umar", Title = "C#"},
+                new BookModel { Id = 3, Author = "Patel",Title ="Php"},
+                new BookModel {Id = 4,Author = "Peter",Title = "java"},
+                new BookModel { Id = 5,Author = "Will",Title = "Dot Net Core"}
+
+            };
+        }
+    }
+}
