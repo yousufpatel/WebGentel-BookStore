c14e1cb02d4f8763ea855d9e6769e2525f60e61b
Md Yousuf Patel <Md_yousuf_Patel@Dellteam.com>
2021-06-03 18:39:56 +0530

    Added endpoinsts and homecontroller

--- src/WebGentel-BookStore/Startup.cs
+++ src/WebGentel-BookStore/Startup.cs
@@ -16,6 +16,7 @@ namespace WebGentel_BookStore
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,14 +28,19 @@ namespace WebGentel_BookStore
             }
 
             app.UseRouting();
-
+           
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapDefaultControllerRoute();
             });
+
+
+            //app.Use(async (context, next) =>
+            //{
+            //    await context.Response.WriteAsync("world");
+            //    await next();
+            //});
+
         }
     }
 }
