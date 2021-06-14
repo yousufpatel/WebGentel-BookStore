9395216324bf725ea1f9c3fb76872d68940605d6
Md Yousuf Patel <Md_yousuf_Patel@Dellteam.com>
2021-06-14 14:28:55 +0530

    View basics

--- src/WebGentel-BookStore/Startup.cs
+++ src/WebGentel-BookStore/Startup.cs
@@ -16,7 +16,7 @@ namespace WebGentel_BookStore
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddControllersWithViews().AddRazorRuntimeCompilation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
